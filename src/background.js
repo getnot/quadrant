133c415536f40a5af0a81d4d98f9ae26002f2d29
prashant shokeen <pshokeen@cvent.com>
2021-06-04 23:48:04 +0530

    intial commit

---
+++ src/background.js
@@ -0,0 +1,14 @@
+
+// fetch("https://bing-image-search1.p.rapidapi.com/images/trending", {
+// 	"method": "GET",
+// 	"headers": {
+// 		"x-rapidapi-key": "024d247477mshd1ab038b4d6f69ap1550c9jsnca4c9180ad2b",
+// 		"x-rapidapi-host": "bing-image-search1.p.rapidapi.com"
+// 	}
+// })
+// .then(response => {
+// 	console.log(response);
+// })
+// .catch(err => {
+// 	console.error(err);
+// });
