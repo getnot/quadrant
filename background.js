4acfb3f3e8be0236f4fe4cf25b20f37584c1543c
prashant shokeen <pshokeen@cvent.com>
2021-06-11 19:04:57 +0530

    image and clock will come when new tab opened

--- background.js
+++ background.js
@@ -1,21 +1,4 @@
 
-// fetch("https://bing-image-search1.p.rapidapi.com/images/trending", {
-// 	"method": "GET",
-// 	"headers": {
-// 		"x-rapidapi-key": "",
-// 		"x-rapidapi-host": "bing-image-search1.p.rapidapi.com"
-// 	}
-// })
-// .then(response => {
-// 	console.log(response);
-// })
-// .catch(err => {
-// 	console.error(err);
-// });
-
-let color = '#3aa757';
-
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color });
-  console.log('Default background color set to %cgreen', `color: ${color}`);
-});
+chrome.action.onClicked.addListener(() => {
+  chrome.tabs.create({ url: "chrome://newtab" })
+})
